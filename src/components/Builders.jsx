456b6182bd85e9f7ec4066f5763d359ec51e55c7
Kalpitha <kalpitharb04@gmail.com>
2023-04-26 13:52:12 +0530

    completed the final alignment of the form and contact form

--- src/components/Builders.jsx
+++ src/components/Builders.jsx
@@ -1,11 +1,11 @@
 import Whyus from './Components/Note.jsx'
 import About from './Components/About.jsx'
 import Next from './Components/About1.jsx';
-import Last from './Components/About2'
-import './AboutUs.css'
+import Last from './Components/About2.jsx'
+import './Builders.css'
 import Footer from './Footer/Footer.jsx'
 
-export default function Aboutus(){
+export default function Builders(){
     return(
         <>
         <div className="About-page">
