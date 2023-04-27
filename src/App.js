85553d8ff7491b6fe6f3ed162b30d374beb8f48d
Kalpitha <kalpitharb04@gmail.com>
2023-04-27 10:04:09 +0530

    Tried adding the responsive basics but that wasnt correct so, decided to do some research adn study on the topic and continue the work so i am commenting the app.js part

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@
 import './App.css';
 // import Footer from './components/Footer/Footer';
 import Builders from './components/Builders';
+// import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  // const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  // useEffect(() => {
+  //   const mediaQuery = window.matchMedia('(max-width: 768px)');
+  //   setIsSmallScreen(mediaQuery.matches);
+
+  //   const handler = (event) => {
+  //     setIsSmallScreen(event.matches);
+  //   };
+
+  //   mediaQuery.addEventListener('change', handler);
+
+  //   return () => {
+  //     mediaQuery.removeEventListener('change', handler);
+  //   };
+  // }, []);
+
   return (
-    <div className="App">
+     <div className="App" >
+    
       <Builders/>
       
     </div>
-  );
+  );//<div className={`App ${isSmallScreen ? 'small-screen' : ''}`}>
 }
 
 export default App;
